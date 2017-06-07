0576fdfa39ddb5f11944adfee798317ee0782d4a
Joe Whitwell <joewhitwell@hotmail.com>
2017-06-07 12:35:17 +0100

    Added rspec text

--- lib/DockingStation.rb
+++
@@ -1,7 +0,0 @@
-class DockingStation
-
-  # def release_bike
-  #
-  # end
-
-end
--- spec/Feature_tests_to_Unit_tests_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'DockingStation.rb'
-
-describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
-end
