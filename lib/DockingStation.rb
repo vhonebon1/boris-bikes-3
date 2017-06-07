31e948f5042c3583e96104835a23e22093f3d5d4
Joe Whitwell <joewhitwell@hotmail.com>
2017-06-07 16:35:01 +0100

    Attr_reader and bike docking

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,9 +1,15 @@
 require_relative "../lib/bike.rb"
 
+
 class DockingStation
+attr_reader :bike
 
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike = bike
   end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,4 +3,21 @@ require 'DockingStation.rb'
 describe DockingStation do
 it { is_expected.to respond_to(:release_bike) }
 
+
+it "releases a bike and expects it to be working" do
+  bike = subject.release_bike
+  expect(bike).to be_working
+end
+
+it "docking station has a method dock_bike taking one argument" do
+  expect(subject).to respond_to(:dock_bike).with(1).argument
+end
+
+it "docks a bike at a station" do
+bike = subject.release_bike
+expect(subject.dock_bike(bike)).to eq bike
+
+end
+
+
 end
