a07c553f654009e0bfc8222a6f891ed3dc28d244
Joe Whitwell <joewhitwell@hotmail.com>
2017-06-07 15:46:15 +0100

    got a bike and expected it to be working

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,9 @@
+require_relative "../lib/bike.rb"
+
 class DockingStation
 
   def release_bike
+    Bike.new
   end
 
 end
--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,5 @@
 class Bike
-def working?
-end
+  def working?
+    true
+  end
 end
